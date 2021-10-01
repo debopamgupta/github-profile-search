0cdcd0089f6470639163567aba7e2bb608634c4f
DG <debopamgupta3000@gmail.com>
2021-10-01 23:22:34 +0530

    Implement Input Box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello world</div>;
+  const [inputVal, setInputVal] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputVal(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputVal);
+  };
+
+  return (
+    <>
+      <div className="App">
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <label htmlFor="Profile Username">Username</label>
+          <br />
+          <input type="text" onChange={(e) => handleInputChange(e)} />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
