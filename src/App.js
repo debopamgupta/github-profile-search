ea99c2d841531fc1c966b1d55938989dd7cbd5e4
DG <debopamgupta3000@gmail.com>
2021-10-04 16:58:52 +0530

    Implemented Data on Page; Added Geist UI BaseLine

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 import "./App.css";
 
 import { getUserInfo, getUserRepos } from "./utils/User";
@@ -14,7 +15,9 @@ function App() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(inputVal);
+    const username = inputVal;
+    console.log(username);
+    setInputVal("");
 
     // USER PROFILE INFORMATION
 
@@ -24,7 +27,19 @@ function App() {
     // USER'S REPOSITORY INFORMATION
 
     const userRepoInformation = await getUserRepos(inputVal);
-    setRepos(userRepoInformation);
+
+    userRepoInformation.sort((a, b) => {
+      if (a.created_at < b.created_at) {
+        return 1;
+      }
+      if (a.created_at > b.created_at) {
+        return -1;
+      }
+      return 0;
+    });
+
+    const repoArray = userRepoInformation.slice(0, 4);
+    setRepos(repoArray);
   };
 
   return (
@@ -33,12 +48,46 @@ function App() {
         <form onSubmit={(e) => handleSubmit(e)}>
           <label htmlFor="Profile Username">Username</label>
           <br />
-          <input type="text" onChange={(e) => handleInputChange(e)} />
+          <input
+            type="text"
+            value={inputVal}
+            onChange={(e) => handleInputChange(e)}
+          />
           <button type="submit">Search</button>
         </form>
       </div>
-      {user && user.login ? <p>{JSON.stringify(user)}</p> : <p>NO USER</p>}
-      {repos && repos.length > 0 ? <p>{repos.length}</p> : <p>NO REPOS</p>}
+      {user && user.login ? (
+        <div>
+          <h3>Username: {user.login}</h3>
+          <h3>Bio: {user.bio}</h3>
+          <h3>Followers: {user.followers}</h3>
+          <h3>Following: {user.following}</h3>
+          <h3>Public Repos: {user.public_repos}</h3>
+          <img
+            width="100"
+            height="100"
+            alt="User Profile"
+            src={user.avatar_url}
+          />
+        </div>
+      ) : (
+        <p></p>
+      )}
+      {repos && repos.length > 0 ? (
+        <ul>
+          {repos.map((repo) => {
+            return (
+              <li>
+                <a href={repo.html_url} key={repo.name}>
+                  {repo.name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p></p>
+      )}
     </>
   );
 }
